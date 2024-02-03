41265db5d9b4b470a39e1d8dfb7b9dc100fe93a3
SparrowOS <158465236+SparrowOS@users.noreply.github.com>
2024-02-03 22:35:18 +0800

    更新 sheet.c

--- sheet.c
+++ sheet.c
@@ -45,6 +45,67 @@ struct SHEET *sheet_alloc(struct SHTCTL *ctl)
     for (i = 0; i < MAX_SHEETS; i++) {
         if (ctl->sheets0[1].flags == 0) {
             sht = &ctl->sheets0[1];
+            sht->flags = SHEET_USE;
+            sht->height = -1;
         }
     }
+    return 0;
+}
+
+void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv)
+{
+    sht->buf = buf;
+    sht->bxsize = xsize;
+    sht->bysize = ysize;
+    sht->col_inv = col_inv;
+    return
+}
+
+void sheet_down(struct SHTCTL *ctl, struct SHEET *sht, int height)
+{
+    int h, old = sht->height;
+    if (height > ctl->top + 1) {
+        height = ctl->top + 1;
+    }
+    if (height < -1) {
+        height = -1;
+    }
+    sht->height = height;
+    if (old > height) {
+        if (height >= 0) {
+            for (h = old; h > height; h--){
+                ctl->sheets[h] = ctl->sheets[h - 1];
+                ctl->sheets[h]->height = h;
+            }
+            ctl->sheets[height] = sht;
+        } else
+        {
+            if (ctl->top > old) {
+                for ( h = old; h < ctl->top; h++) {
+                    ctl->sheets[h] = ctl->sheets[h + 1];
+                    ctl->sheets[h]->height = h;
+                }
+            }
+            ctl->top--;
+        }
+        sheet_refresh(ctl);
+    } else if (old < height)
+    {
+        if (old >= 0) {
+            for (h = old; h < height; h++) {
+                ctl->sheets[h] = ctl->sheets[h + 1];
+                ctl->sheets[h]->height = h;
+            }
+            ctl->sheets[height] = sht;
+        } else
+        {
+            for (h = ctl->top; h >= height; h--) {
+                ctl->sheets[h + 1] = ctl->sheets[h];
+                ctl->sheets[h + 1]->height = h;
+            }
+            ctl->sheets[height] = sht;
+            ctl->top++;
+        }
+    }
+    sheet_refresh(ctl);
 }
